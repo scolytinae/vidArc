4ccda5aeef25627fdfc5295cb9c18ac9187ee062
Igor <mamajaa@gmail.com>
2014-08-21 00:42:23 +0400

    add simple database script

---
+++ schema.sql
@@ -0,0 +1,13 @@
+drop table if exists films;
+
+create table films(
+  id integer primary key autoincrement,
+  caption text not null,
+  description text,
+  director text,
+  filename text not null,
+  torrent_file text not null
+);
+
+
+
