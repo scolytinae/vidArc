d1e17a614eb9c604e678075447b0611324ebd8df
Tkachev Igor <mamajaa@gmail.com>
2014-09-12 14:57:44 +0400

    f** design

--- views/general.py
+++ views/general.py
@@ -1,16 +1,26 @@
+#--* coding: utf-8 *--
 from flask import Blueprint, render_template
 
 mod = Blueprint("general", __name__)
 
+LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi."
+
+FILMS = [
+    {'title': u'Первый мститель: Другая война', 'director': 'Tkachev Igor', 'story': LOREM_IPSUM},
+    {'title': 'hello2', 'director': 'Tkachev Igor', 'story': LOREM_IPSUM}
+]
+
+
 @mod.route("/")
 def index():
-    return render_template("general/index.html", name="Igor")
+    return render_template("general/index.html", items=FILMS)
+
 
 @mod.route("/films")
 def film_list():
-    return render_template("general/film_list.html")
+    return render_template("general/film_list.html", films=FILMS)
+
 
 @mod.route("/films/<string:film_page>")
 def film(film_page):
-    f = {'title': 'hello', 'director': 'Tkachev Igor', 'story': 'asdfasdfasdfasdasdfasdfasdfasdfas asdfasdfas'}
-    return render_template("general/film.html", film=f)
+    return render_template("general/film.html", film=FILMS[0])
